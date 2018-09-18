bcdc9e5f270e4f0609213b65a24c4c217abdb0e0
easonla <yhh281@nyu.edu>
2018-09-18 12:32:32 +0800

    add answer to Q2-3

--- ngram.py
+++ ngram.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.util import ngrams
+import operator
 
 def word_grams(words, n):
     grams = {}
@@ -16,7 +17,7 @@ def prob(grams):
         grams_prob[gram] = grams_prob[gram]/d
     return grams_prob
 
-#Quiz 2
+#Quiz 2-1
 def ngram_probs(filenme='data/raw_sentences.txt'):
     with open('data/raw_sentences.txt', 'r') as f:
         w = ' '.join(s.strip().lower() for s in f.readlines())
@@ -24,7 +25,7 @@ def ngram_probs(filenme='data/raw_sentences.txt'):
     trigrams = word_grams(w.split(' '),3)
     return prob(bigrams),prob(trigrams)
 
-#Quiz3
+#Quiz 2-2
 def prob3(bigram, cnt2=cnt2, cnt3=cnt3):
     if bigram not in cnt2: return None
     else : prob = cnt2[bigram]
@@ -33,3 +34,18 @@ def prob3(bigram, cnt2=cnt2, cnt3=cnt3):
         if p[:2]==bigram: 
             next_word_prob[p[2]] = cnt2[bigram]/cnt3[p]
     return next_word_prob
+
+#Quiz 2-3
+def predict_max(starting, cnt2=cnt2, cnt3=cnt3):
+    sentence = []
+    sentence.append(starting)
+    while True:
+        next_word_prob = prob3(starting)
+        next_word = max(next_word_prob.items(), key=operator.itemgetter(1))[0]
+        if len(sentence) < 15 and next_word != ".":
+            sentence.append(next_word)
+            starting = (starting[1],next_word)
+        else:
+            break
+    return sentence
+
